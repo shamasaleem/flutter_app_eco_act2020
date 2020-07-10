4e2553567a48b86a834a884ead99d8fa093b8003
Shama <shama4776@gmail.com>
2020-07-10 11:05:53 -0700

    Commit 9

--- lib/screens/create_your_own_event.dart
+++ lib/screens/create_your_own_event.dart
@@ -9,37 +9,67 @@ class _CreateOwnYourEventState extends State<CreateOwnYourEvent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 10.0),
-            Text ('Create Your Own Event',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-            ),
-          Stack(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
             children: <Widget>[
-              Container(
-                decoration: BoxDecoration (
-                color: Colors.lightBlueAccent.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.35),
-                    spreadRadius: 5,
-                    blurRadius:7,
-                    offset: Offset(0,3),
+              SizedBox(height: 10.0),
+              Text ('Create Your Own Event',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
               ),
-                height: 615.0,
-                width: 350.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(11.0),
-                child: Container(
+            Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration (
+                  color: Colors.lightBlueAccent.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.35),
+                      spreadRadius: 5,
+                      blurRadius:7,
+                      offset: Offset(0,3),
+                    ),
+                  ],
+                ),
+                  height: 615.0,
+                  width: 350.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(11.0),
+                  child: Container(
+                      decoration: BoxDecoration (
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.35),
+                            spreadRadius: 5,
+                            blurRadius:7,
+                            offset: Offset(0,3),
+                          ),
+                        ],
+                      ),
+                      height: 75.0,
+                      width: 325.0,
+                      child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              labelText: 'Name of Event: ',
+                            ),
+                          )
+                      )
+                  ),
+                ),
+                Positioned(
+                  top: 115,
+                  left: 10,
+                  child: Container(
                     decoration: BoxDecoration (
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(15.0),
@@ -52,197 +82,169 @@ class _CreateOwnYourEventState extends State<CreateOwnYourEvent> {
                         ),
                       ],
                     ),
-                    height: 75.0,
-                    width: 325.0,
+                    height: 75,
+                    width: 325,
                     child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: TextField(
                           obscureText: false,
                           decoration: InputDecoration(
-                            labelText: 'Name of Event: ',
+                            labelText: 'Location: ',
                           ),
                         )
-                    )
-                ),
-              ),
-              Positioned(
-                top: 115,
-                left: 10,
-                child: Container(
-                  decoration: BoxDecoration (
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.35),
-                        spreadRadius: 5,
-                        blurRadius:7,
-                        offset: Offset(0,3),
-                      ),
-                    ],
-                  ),
-                  height: 75,
-                  width: 325,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          labelText: 'Location: ',
-                        ),
-                      )
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 225,
-                left: 10,
-                child: Container(
-                  decoration: BoxDecoration (
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.35),
-                        spreadRadius: 5,
-                        blurRadius:7,
-                        offset: Offset(0,3),
-                      ),
-                    ],
-                  ),
-                  height: 75,
-                  width: 325,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          labelText: 'Date: ',
-                        ),
-                      )
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 335,
-                left: 10,
-                child: Container(
-                  decoration: BoxDecoration (
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.35),
-                        spreadRadius: 5,
-                        blurRadius:7,
-                        offset: Offset(0,3),
-                      ),
-                    ],
-                  ),
-                  height: 75,
-                  width: 325,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          labelText: 'Time: ',
-                        ),
-                      )
-                  ),
-                ),
-              ),
-
-              Positioned(
-                top: 435,
-                left: 10,
-                child: Container(
-                  decoration: BoxDecoration (
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.35),
-                        spreadRadius: 5,
-                        blurRadius:7,
-                        offset: Offset(0,3),
-                      ),
-                    ],
-                  ),
-                  height: 100,
-                  width: 325,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          labelText: 'Event Description: ',
-                        ),
-                      )
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 550,
-                left: 125,
-                child: Container(
-                  decoration: BoxDecoration (
-                    color: Colors.teal.withOpacity(0.75),
-                    borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.35),
-                        spreadRadius: 5,
-                        blurRadius:7,
-                        offset: Offset(0,3),
-                      ),
-                    ],
-                  ),
-                  height: 45,
-                  width: 100,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(' Create',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
                     ),
                   ),
                 ),
-              )
-
-            ],
-          ),
-            SizedBox(height: 15.0),
-            Container(
-              decoration: BoxDecoration (
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 5,
-                    blurRadius:7,
-                    offset: Offset(0,3),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('                Bottom Navigation Bar',
-                  style: TextStyle(
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                Positioned(
+                  top: 225,
+                  left: 10,
+                  child: Container(
+                    decoration: BoxDecoration (
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.35),
+                          spreadRadius: 5,
+                          blurRadius:7,
+                          offset: Offset(0,3),
+                        ),
+                      ],
+                    ),
+                    height: 75,
+                    width: 325,
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            labelText: 'Date: ',
+                          ),
+                        )
+                    ),
                   ),
                 ),
-              ),
-              height: 35.0,
-              width: 350.0,
-            )
-          ],
+                Positioned(
+                  top: 335,
+                  left: 10,
+                  child: Container(
+                    decoration: BoxDecoration (
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.35),
+                          spreadRadius: 5,
+                          blurRadius:7,
+                          offset: Offset(0,3),
+                        ),
+                      ],
+                    ),
+                    height: 75,
+                    width: 325,
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            labelText: 'Time: ',
+                          ),
+                        )
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  top: 435,
+                  left: 10,
+                  child: Container(
+                    decoration: BoxDecoration (
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.35),
+                          spreadRadius: 5,
+                          blurRadius:7,
+                          offset: Offset(0,3),
+                        ),
+                      ],
+                    ),
+                    height: 100,
+                    width: 325,
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            labelText: 'Event Description: ',
+                          ),
+                        )
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 550,
+                  left: 125,
+                  child: Container(
+                    decoration: BoxDecoration (
+                      color: Colors.teal.withOpacity(0.75),
+                      borderRadius: BorderRadius.circular(15.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.35),
+                          spreadRadius: 5,
+                          blurRadius:7,
+                          offset: Offset(0,3),
+                        ),
+                      ],
+                    ),
+                    height: 45,
+                    width: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(' Create',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+              SizedBox(height: 15.0),
+              Container(
+                decoration: BoxDecoration (
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius:7,
+                      offset: Offset(0,3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('                Bottom Navigation Bar',
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                height: 35.0,
+                width: 350.0,
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/other_screen.dart
+++ lib/screens/other_screen.dart
@@ -9,79 +9,88 @@ class OtherScreen extends StatefulWidget {
 class _OtherScreenState extends State<OtherScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Stack(
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/backgroundimage.png"), fit: BoxFit.cover)),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/backgroundimage.png"), fit: BoxFit.cover)),
 
 
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
 
-          ),
-        ),
-        Positioned(
-          top: 40,
-          left: 10,
-          right: 10,
-          child: Column (
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Center(
-                child: Text('If other, please explain',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
               ),
-              SizedBox (height: 350),
-              Container(
-                decoration: BoxDecoration (
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12.withOpacity(0.2),
-                      spreadRadius: 5,
-                      blurRadius:7,
-                      offset: Offset(0,3),
+            ),
+            Positioned(
+              top: 40,
+              left: 10,
+              right: 10,
+              child: Column (
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Center(
+                    child: Text('If other, please explain',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
-                  ],
-                ),
-
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child:TextField(
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      labelText: 'Please Explain',
-                    ),
-                  )
-                ),
-                height: 200,
-                width: 300,
-
-              ),
-              SizedBox(height: 30),
-              GestureDetector(
-                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ChooseYourFeed()),),
-                child: Text('Next/Skip',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
                   ),
-                ),
+                  SizedBox (height: 350),
+                  Container(
+                    decoration: BoxDecoration (
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius:7,
+                          offset: Offset(0,3),
+                        ),
+                      ],
+                    ),
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child:TextField(
+                        obscureText: false,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          labelText: 'Please Explain',
+                        ),
+                      )
+                    ),
+                    height: 200,
+                    width: 300,
+
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ChooseYourFeed()),),
+                    child: Text('Next/Skip',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ],
+            ),
+          ],
     ),
+        ),
+      ),
     );
   }
 }
